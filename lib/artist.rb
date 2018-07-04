ad5844713302b6433c29f7401398d54f6e19ba6e
Nate Miller <natekmiller96@gmail.com>
2018-07-04 02:35:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ class Artist
     @genres = []
     @@all << self
   end
-  def new_song(name, artist, genre)
+  def new_song(name, genre)
     song = Song.new(name, self, genre)
     @songs << song
     @genres << song.genre
