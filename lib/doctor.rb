f01585ad399a1231b88790ab50f996f60ac63506
Nate Miller <natekmiller96@gmail.com>
2018-07-04 02:47:16 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,11 @@
 class Doctor
+  @@all = []
   attr_accessor :name, :patients, :appointments
   def initialize(name)
     @name = name
     @patients = []
     @appointments = []
+    @@all << self
   end
   def new_appointment(date, patient)
     appointment = Appointment.new(date, patient, self)
